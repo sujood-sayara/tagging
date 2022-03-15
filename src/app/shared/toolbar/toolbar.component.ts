012cbf16f1646aa7f9d0d2ea0e0245446600825c
sujood-sayara <sujoodahmadsayara@gmail.com>
2022-03-15 13:01:17 +0200

    add filter to toolbar

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,43 +1,49 @@
 import {
   Component,
   EventEmitter,
-  Input,
   OnInit,
   Output,
-  OnChanges,
-  SimpleChanges,
 } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { map } from 'rxjs/operators';
 import { Tag } from 'src/models/tag';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { Store } from '@ngrx/store';
+import { tagState } from 'src/app/reducers/tag.reducer';
+import { FormControl } from '@angular/forms';
+import { LoadTagAction } from 'src/actions/tag.action';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.css'],
 })
-export class ToolbarComponent implements OnInit, OnChanges {
-  @Input() tags$;
+export class ToolbarComponent implements OnInit {
+
   @Output() selectedTag = new EventEmitter();
+  filterInput = new FormControl('');
+  dataSource = []
   originalTags: Tag[] = [];
-  numberOfPages;
+  numberOfPages = 0;
   page = 0;
   selectedTagName: any;
   isSelected = false;
-  constructor() {}
-  ngOnChanges(changes: SimpleChanges): void {
-    this.tags$.subscribe((data) => {
-      this.originalTags = data;
-      if (this.originalTags !== undefined) {
-        this.numberOfPages = Math.ceil(this.originalTags.length / 5);
-        this.slide(0);
-      }
-    });
-    this.selectedTagName = null;
-  }
+  constructor(private tagStore: Store<tagState>,) { }
+  ngOnInit(): void {
+    this.tagStore
+      .select((store: any) => store.tag.tags)
+      .subscribe((data) => {
+        if (data === undefined) {
+          this.tagStore.dispatch(new LoadTagAction());
+        }
+        else {
+          this.originalTags = data;
+          this.numberOfPages = Math.ceil(this.originalTags.length / 5);
+          this.slide(0, this.originalTags);
+        }
+      });
 
-  ngOnInit(): void {}
+    this.filterInput.valueChanges.subscribe((value) => {
+      this.applyFilter(value);
+    });
+  }
   selectdTagValue(tag) {
     this.selectedTagName = tag.tagName;
     this.selectedTag.emit(tag);
@@ -46,20 +52,18 @@ export class ToolbarComponent implements OnInit, OnChanges {
     this.selectedTagName = null;
     this.selectedTag.emit(undefined);
   }
-  slide(index) {
-    this.tags$ = this.tags$.pipe(
-      map(() => this.originalTags.slice(index * 5, index * 5 + 5))
-    );
+  slide(index, originalTags) {
+    this.dataSource = originalTags.slice(index * 5, index * 5 + 5)
   }
   nextPage() {
     if (this.page < this.numberOfPages - 1) this.page++;
     else this.page = 0;
-    this.slide(this.page);
+    this.slide(this.page, this.originalTags);
   }
-  prePage() {
+  prevPage() {
     if (this.page > 0) this.page--;
     else this.page = this.numberOfPages - 1;
-    this.slide(this.page);
+    this.slide(this.page, this.originalTags);
   }
   calculateClasses(tag) {
     if (tag.tagName === this.selectedTagName) {
@@ -71,5 +75,13 @@ export class ToolbarComponent implements OnInit, OnChanges {
     return {
       selectedSpan: this.isSelected,
     };
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource = this.originalTags.filter((tag) =>
+      tag.tagName.toLowerCase().includes(filterValue.toLowerCase())
+    )
+    this.numberOfPages = Math.ceil(this.dataSource.length / 5);
+    this.slide(0, this.dataSource)
+
   }
 }
